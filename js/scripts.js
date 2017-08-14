bfd03c69a8b02836b5ba4189b7c6d017df59fee0
Elrey Belmonti <elbelmonti@gmail.com>
2017-08-13 17:51:32 -0700

    add error messages for invalid value

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $("#triForm").submit(function(event){
+    event.preventDefault();
     var side1Input = parseInt($("#side1").val());
     var side2Input = parseInt($("#side2").val());
     var side3Input = parseInt($("#side3").val());
@@ -7,6 +8,21 @@ $(document).ready(function(){
     console.log(side1Input);
     console.log(side2Input);
     console.log(side3Input);
+
+    if (isNaN(side1Input)) {
+      $(".side1").addClass("has-error");
+      $("#empty1, .empty").show();
+      return;
+    } else if (isNaN(side2Input)) {
+      $(".side2").addClass("has-error");
+      $("#empty2, .empty").show();
+      return;
+    } else if (isNaN(side3Input)) {
+      $(".side3").addClass("has-error");
+      $("#empty3, .empty").show();
+      return;
+    }
+
     $("#triForm, .thumbnail, .panel, .page-header").hide();
 // for debugging
     var equilateral = (side1Input === side2Input) && (side2Input === side3Input);
@@ -25,6 +41,6 @@ $(document).ready(function(){
     } else if (isosceles) {
       $(".two-equal").show();
     }
-      event.preventDefault();
+
   });
 });
